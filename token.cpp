c637f6ccb1af0035a6abd1daea17e42ce43d2166
Navin Ayer <navinayer56@gmail.com>
2015-08-19 22:54:16 +0545

    small changes

--- token.cpp
+++ token.cpp
@@ -17,6 +17,8 @@ int main(){
     ourRegex.add("ip_address" , "([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.([0-9]+)(::([0-9]+)|)") ;
     ourRegex.add("date","(199[0-9]|200[0-9])-([1-9]|0[1-9]|1[012])-([1-9]|[0-2][1-9]|3[01])") ;
     ourRegex.add("url","([a-zA-Z]+)\\.([a-zA-Z]+)\\.([a-zA-Z]+)") ;
+    ourRegex.add("time","(([0-1][0-9])|([2][0-3])):([0-5][0-9]):([0-5][0-9])") ;
+    ourRegex.add("file","(http|ftp|http\://|/)[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\\S*)") ;
 
     string word = "cleveland.directrouter.com 192.168.0.1 - - [ 2007-03-14 23:57:05 -0800] \"GET /fotos/lenew.jpg HTTP/1.0\" 200 1241 \"-\" \"-\"" ;
 
