33eb36dce5e4b5411047a2ed0e1ff8cb2d0d9643
Pramod <unknown.emlap@gmail.com>
2015-08-20 18:45:36 +0545

    error seg fault

---
+++ HMM.h
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <map>
+#include <utility>
+#include <list>
+#include <string>
+
+using namespace std;
+
+template<class T>
+class HMM{
+private:
+	map<string, int> tags;
+	map<string, double> probtt, probwt;
+	
+public:
+	HMM() {}
+	void Train(vector<vector<T>> & tlogs){
+		for(auto i=tlogs.begin(); i != tlogs.end(); ++i){
+			for(auto j= i->begin(); j != i->end(); ++j){
+				map<string, int>::iterator it = tags.find(j->second);
+				if( it != tags.end())
+					it->second = it->second + 1;
+				else{
+					tags.insert(make_pair(j->second, 1));
+				}
+			}
+		}
+
+		//calculation of transition probability
+		for(auto i=tlogs.begin(); i != tlogs.end(); ++i){
+			for(auto j=i->begin(); j!=(i->end()-1); ++j){
+				string tt = j->second + (j+1)->second;/*
+				map<string, int>::iterator it = probtt.find(tt);
+				if(it != probtt.end()){
+					it->second += 1/tags[j->second];
+				}
+				else{
+					probtt.insert(make_pair(tt, 1/tags[j->second]));
+				}
+				*/
+				cout<<tt<<"\n";
+			}
+		}
+
+/*
+		for(auto i=tags.begin(); i != tags.end(); ++i){
+			cout<<i->first<"\t";
+			cout<<i->second<<"\n";
+		}
+*/
+	}
+
+};
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "HMM.h"
+
+using namespace std;
+
+typedef pair<int, string> T;
+
+vector<T> tokenize(string s) {
+	istringstream is(s);
+	vector<T> res;
+	string t;
+	for (int i = 0; is >> t; ++i) {
+		res.emplace_back(i, t);
+	}
+	return res;
+}
+
+vector<vector<T>> 
+read(const char * file_name) {
+	ifstream file(file_name);
+	string s;
+	vector<vector<T>> res;
+	while (getline(file, s)) {
+		res.push_back(tokenize(s));
+	}
+	return res;
+}
+
+int main(int cnt, char * args[])
+{
+	if(cnt<3){
+		cout<<"Hidden Markov Model Implementation\n";
+		cout<<"Usage: Program-name training-file tagging-file\n";
+	}
+	else{
+		vector<vector<T>> Tfile = read(args[1]);
+		vector<vector<T>> logs = read(args[2]);
+		HMM<T> hmm;
+		hmm.Train(Tfile);
+	}
+}
